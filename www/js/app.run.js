dad4d2c61852c01ad92191dfe68e3d95cf7500f9
mouylenglim <mouyleng@instedd.org>
2016-10-31 13:46:26 +0700

    font and design include

--- www/js/app.run.js
+++ www/js/app.run.js
@@ -7,14 +7,17 @@ runBlock.$inject = ['$ionicPlatform', '$cordovaSQLite', '$cordovaFile', "$rootSc
 var db = null;
 
 function runBlock ($ionicPlatform, $cordovaSQLite, $cordovaFile, $rootScope, $location) {
+
   $ionicPlatform.ready(function() {
-    console.log('run');
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
+
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
+
 }
--- www/js/grades/grades-controllers.js
+++ www/js/grades/grades-controllers.js
@@ -14,15 +14,15 @@ angular.module('app')
 
   $ionicPlatform.ready(function() {
     $cordovaFile.copyFile(cordova.file.applicationDirectory + 'www/', "khmer-writing.db", cordova.file.dataDirectory, "khmer-writing.db")
-     .then(function (success) {
-       db = $cordovaSQLite.openDB({ name: "khmer-writing.db"});
-       GradesServices.all().then(function (grades) {
-         vm.grades = grades;
-         $state.go('grades');
-       });
-     }, function (error) {
-       console.log('error file transfer : ', error);
-     });
-   })
+      .then(function (success) {
+        db = $cordovaSQLite.openDB({ name: "khmer-writing.db"});
+        GradesServices.all().then(function (grades) {
+          vm.grades = grades;
+          $state.go('grades');
+        });
+    }, function (error) {
+      console.log('error file transfer : ', error);
+    });
+  })
 
 })
