cf3bb939212b6ada7b163c469d192b63140394c3
mouylenglim <mouyleng@instedd.org>
2016-12-05 14:36:52 +0700

    add user content ui

--- www/js/users/users-controller.js
+++ www/js/users/users-controller.js
@@ -2,14 +2,4 @@ angular.module('app')
 
 .controller('UsersCtrl', function($scope, $ionicPopup) {
   var vm = $scope;
-
-  showUsers();
-
-  function showUsers() {
-
-    var myPopup = $ionicPopup.show({
-      templateUrl: 'templates/users.html',
-      scope: vm
-    });
-  }
 })
