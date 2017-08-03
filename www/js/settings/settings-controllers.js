82e696a93afe4b31d30f6b631bf99c3219ff69a6
mouyleng <mouyleng@instedd.org>
2017-08-03 14:22:42 +0700

    fix setting ui for edit user

--- www/js/settings/settings-controllers.js
+++ www/js/settings/settings-controllers.js
@@ -16,7 +16,7 @@ angular.module('app')
   vm.isGuestUser = function(){
     return vm.currentUser.id == 1;
   }
-  vm.soundStyle = {"width": "35%", "margin-left": vm.isGuestUser() ? "20%" : "0%"};
+  vm.soundStyle = {"margin-left": vm.isGuestUser() ? "14%" : "1%"};
 
   var popup;
 
